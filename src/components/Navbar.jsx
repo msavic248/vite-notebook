e5a3a8589a7de10c5a1eb7234ce6a1fdd9931245
Milos Savic <msavic248@yahoo.com>
2022-12-20 14:09:12 +0100

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,28 @@ function Navbar(props) {
     const {user, loggedIn, onLoginClick, onLogoutClick} = props;
 
     return (<nav>
-        <h1>
-            <Link to="/">Home</Link>
-        </h1>
-        {loggedIn ? (
-            <div className="dropdown">
-                <img src={user.identities[0].identity_data.avatar_url} width="32px" height="32px"/>
-                <div className="dropdown-content">
-                    <p>Profile</p>
-                    <p>Settings</p>
-                    <button onClick={onLogoutClick}>Sign out</button>
+        <h1>Vite Notebook</h1>
+        <div className="nav-options">
+            {loggedIn ? (
+                <>
+                <h2>
+                    <Link to="/">Home</Link>
+                </h2>
+                <div className="dropdown">
+                    <img src={user.identities[0].identity_data.avatar_url} width="32px" height="32px"/>
+                    <div className="dropdown-content">
+                        <p>Profile</p>
+                        <p>Settings</p>
+                        <button onClick={onLogoutClick}>Sign out</button>
+                    </div>
                 </div>
-            </div>
-            
-        ) : (
-            <button onClick={onLoginClick}>Sign in with Google</button>
-        )}
+                </>
+            ) : (
+                <div className="dropdown">
+                    <button onClick={onLoginClick}>Sign in with Google</button>
+                </div>
+            )}
+        </div>
     </nav>
     )
 }
